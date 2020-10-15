b754a2f940a76d1d93d88a5e7b4f7aca0f3bf2f8
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-10-15 21:32:39 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201015211824) do
+ActiveRecord::Schema.define(version: 20201015212238) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -21,10 +21,10 @@ ActiveRecord::Schema.define(version: 20201015211824) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.string   "published_status"
+    t.string   "post_status"
   end
 
 end
